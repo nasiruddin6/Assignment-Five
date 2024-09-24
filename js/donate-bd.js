15b898bdfeef8c69de8195c37434888998163748
nasiruddin6 <mdnasirislam208@gmail.com>
2024-09-25 01:26:07 +0600

    Minus Tk &  Added Taka

--- js/donate-bd.js
+++ js/donate-bd.js
@@ -1,3 +1,66 @@
+// Section One 
+document.getElementById('DonateNowBtnOneSection').addEventListener("click", function(){
+  const NavbarBalance=getTextFieldValueById('navBalance')
+  const BalanceOneSectionPart=getTextFieldValueById('BalanceOneSection')
+  const TitleOneSection=getTextFieldValueById('titleOneSection')
+  const InputMoneyOneSectionPart=getInputFieldValueById('InputMoneyOneSection')
+
+  const MinusNavbarMoney = NavbarBalance - InputMoneyOneSectionPart;
+  const AddedMoneyOneSection = BalanceOneSectionPart + InputMoneyOneSectionPart;
+
+    if (InputMoneyOneSectionPart <= NavbarBalance && InputMoneyOneSectionPart > 0) {
+      document.getElementById('navBalance').innerText =
+      MinusNavbarMoney;
+      document.getElementById('BalanceOneSection').innerText =  AddedMoneyOneSection;
+    } 
+    else {
+      alert("Error ! You don't have this amount in your account");
+    }
+
+})
+
+// Section Two 
+document.getElementById('DonateNowBtnTwoSection').addEventListener("click", function(){
+  const NavbarBalance=getTextFieldValueById('navBalance')
+  const BalanceTwoSectionPart=getTextFieldValueById('BalanceTwoSection')
+  const TitleTwoSection=getTextFieldValueById('titleTwoSection')
+  const InputMoneyTwoSectionPart=getInputFieldValueById('InputMoneyTwoSection')
+
+  const MinusNavbarMoney = NavbarBalance - InputMoneyTwoSectionPart;
+  const AddedMoneyTwoSection = BalanceTwoSectionPart + InputMoneyTwoSectionPart;
+
+    if (InputMoneyTwoSectionPart <= NavbarBalance && InputMoneyTwoSectionPart > 0) {
+      document.getElementById('navBalance').innerText =
+      MinusNavbarMoney;
+      document.getElementById('BalanceTwoSection').innerText =  AddedMoneyTwoSection;
+    } 
+    else {
+      alert("Error ! You don't have this amount in your account");
+    }
+
+})
+
+// Section Three 
+document.getElementById('DonateNowBtnThreeSection').addEventListener("click", function(){
+  const NavbarBalance=getTextFieldValueById('navBalance')
+  const BalanceThreeSectionPart=getTextFieldValueById('BalanceThreeSection')
+  const TitleThreeSection=getTextFieldValueById('titleThreeSection')
+  const InputMoneyThreeSectionPart=getInputFieldValueById('InputMoneyThreeSection')
+
+  const MinusNavbarMoney = NavbarBalance - InputMoneyThreeSectionPart;
+  const AddedMoneyThreeSection = BalanceThreeSectionPart + InputMoneyThreeSectionPart;
+
+    if (InputMoneyThreeSectionPart <= NavbarBalance && InputMoneyThreeSectionPart > 0) {
+      document.getElementById('navBalance').innerText =
+      MinusNavbarMoney;
+      document.getElementById('BalanceThreeSection').innerText =  AddedMoneyThreeSection;
+    } 
+    else {
+      alert("Error ! You don't have this amount in your account");
+    }
+
+})
+
 // Donate Button 
 document.getElementById('donationBtn').addEventListener('click', function(){
   const OpenDonationPage=allUnhide('AllDonatePart')
@@ -14,3 +77,24 @@ document.getElementById('historyBtn').addEventListener('click', function(){
   const donationBtnColor=document.getElementById('donationBtn').classList.remove('bg-[#B4F461]')
   const historyBtnColor=document.getElementById('historyBtn').classList.add('bg-[#B4F461]')
 })
+
+
+// document.getElementById('DonateNowBtnOneSection').addEventListener("click", function(){
+//   const NavbarBalance=getTextFieldValueById('navBalance')
+//   const BalanceOneSectionPart=getTextFieldValueById('BalanceOneSection')
+//   const TitleOneSection=getTextFieldValueById('titleOneSection')
+//   const InputMoneyOneSectionPart=getTextFieldValueById('InputMoneyOneSection')
+
+//   const MinusNavbarMoney = NavbarBalance - InputMoneyOneSectionPart;
+//     const AddedMoneyOneSection = BalanceOneSectionPart + InputMoneyOneSectionPart;
+
+//     if (InputMoneyOneSectionPart <= NavbarBalance && InputMoneyOneSectionPart > 0) {
+//       document.getElementById('navBalance').innerText =
+//       MinusNavbarMoney;
+//       document.getElementById('BalanceOneSection').innerText =  AddedMoneyOneSection;
+//     } 
+//     else {
+//       alert("Error ! You don't have this amount in your account");
+//     }
+
+// })
