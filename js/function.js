0c07072181e68f5fc9df9596886aae4b558f5716
nasiruddin6 <mdnasirislam208@gmail.com>
2024-09-24 23:51:35 +0600

    Creat a New file & add all html property

--- js/function.js
+++ js/function.js
@@ -1,14 +1,22 @@
-//  work-02 money
 
+// all-section & transaction hidden
+function allUnhide(hide){
+    const transactionsHistory =document.getElementById('HistoryInfo').classList.add('hidden')
+    const allSectionForm =document.getElementById('all-section-form').classList.add('hidden')
+
+    const unhide =document.getElementById(hide).classList.remove('hidden')
+
+}
+
+
+//  work-02 money
 function getInputFieldValueById(id){
     const inputValue = document.getElementById(id).value;
     const inputNumber =parseFloat(inputValue);
     return inputNumber;
 }
 
-
-// balance add & minus
-
+// balance add & out
 function getTextFieldValueById(id){
 const textValue = document.getElementById(id).innerText;
 const textNumber =parseFloat(textValue);
